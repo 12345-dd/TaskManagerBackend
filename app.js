ded97a13a70db412944f9e13248e181b575f6f34
12345-dd <mayurpatil98607@gmail.com>
2025-05-12 11:26:13 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager").then(()=>{
+mongoose.connect("mongodb+srv://mayurpatil98607:mayur123@cluster0.kulyrnk.mongodb.net/task-manager").then(()=>{
     console.log("Database is Connected");
 }).catch((err)=>{
     console.log(`Error in Connected database - ${err}`)
